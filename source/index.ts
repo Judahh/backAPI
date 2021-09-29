96c93c926f6ed57fcb490b6b8b8b413aad2bbeee
Judahh <judahholanda@gmail.com>
2021-09-29 15:11:51 -0300

    update packages

--- source/index.ts
+++ source/index.ts
@@ -1,9 +1,8 @@
 import { Mixin } from 'ts-mixer';
 
-import BasicService from './service/basicService';
 import DatabaseHandler from './database/databaseHandler';
 import IDatabaseHandler from './database/iDatabaseHandler';
 import IRouter from './router/iRouter';
 
-export { DatabaseHandler, BasicService, Mixin };
+export { DatabaseHandler, Mixin };
 export type { IRouter, IDatabaseHandler };
