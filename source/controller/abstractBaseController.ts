9bd40d546d4c711488073ab908754e5f9428096a
Judah <judahholanda7@gmail.com>
2022-01-09 19:12:40 -0300

    update headers

--- source/controller/abstractBaseController.ts
+++ source/controller/abstractBaseController.ts
@@ -345,12 +345,12 @@ export default abstract class AbstractBaseController extends Default {
           process.env.ALLOWED_HEADERS
             ? process.env.ALLOWED_HEADERS
             : 'Access-Control-Allow-Headers, Origin, Accept, ' +
-                'X-Requested-With, Content-Type, Access-Control-Request-Method, ' +
-                'Access-Control-Request-Headers,  Authorization, authorization, pages, ' +
-                'page, pageSize, numberOfPages, pagesize, numberofpages, pageNumber, ' +
+                'X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, ' +
+                'Authorization, authorization, Access-Control-Allow-Origin, ' +
+                'pages, page, pageSize, numberOfPages, pagesize, numberofpages, pageNumber, ' +
                 'pagenumber, type, token, filter, single, sort, sortBy, sortByDesc, ' +
                 'sortByDescending, sortByAsc, sortByAscending, sortByDescending, ' +
-                'Access-Control-Allow-Origin'
+                'correct, replace, id, name, description, createdAt, updatedAt'
         );
       }
       await this.emit(responseOrSocket, operation, 200, {});
